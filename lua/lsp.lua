863ff1d45dabc7fd590bb3c644b0c3dbcaffc0e7
ZacharyRizer <zacharyrizer@gmail.com>
2024-02-12 16:41:51 -0500

    Few small kinks

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -6,20 +6,21 @@ return {
         config = function()
             local lspconfig = require("lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local opts = { noremap = true, silent = true }
 
-            local on_attach = function()
-                -- set keybinds
-                A.map("n", "gr", ":Telescope lsp_references<CR>", A.opts.ns)
-                A.map("n", "gd", ":Telescope lsp_definitions<CR>", A.opts.ns)
-                A.map("n", "gi", ":Telescope lsp_implementations<CR>", A.opts.ns)
-                A.map("n", "gt", ":Telescope lsp_type_definitions<CR>", A.opts.ns)
-                A.map("n", "<Leader>la", vim.lsp.buf.code_action, A.opts.ns)
-                A.map("n", "<Leader>ld", ":Telescope diagnostics bufnr=0<CR>", A.opts.ns)
-                A.map("n", '<Leader>ls', ':Telescope treesitter<CR>', A.opts.ns)
-                A.map("n", "<Leader>rn", vim.lsp.buf.rename, A.opts.ns)
-                A.map("n", "[d", vim.diagnostic.goto_prev, A.opts.ns)
-                A.map("n", "]d", vim.diagnostic.goto_next, A.opts.ns)
-                A.map("n", "K", vim.lsp.buf.hover, A.opts.ns)
+            local on_attach = function(_, bufnr)
+                opts.buffer = bufnr
+                A.map("n", "gr", ":Telescope lsp_references<CR>", opts)
+                A.map("n", "gd", ":Telescope lsp_definitions<CR>", opts)
+                A.map("n", "gi", ":Telescope lsp_implementations<CR>", opts)
+                A.map("n", "gt", ":Telescope lsp_type_definitions<CR>", opts)
+                A.map("n", "<Leader>la", vim.lsp.buf.code_action, opts)
+                A.map("n", "<Leader>ld", ":Telescope diagnostics bufnr=0<CR>", opts)
+                A.map("n", '<Leader>ls', ':Telescope treesitter<CR>', opts)
+                A.map("n", "<Leader>rn", vim.lsp.buf.rename, opts)
+                A.map("n", "[d", vim.diagnostic.goto_prev, opts)
+                A.map("n", "]d", vim.diagnostic.goto_next, opts)
+                A.map("n", "K", vim.lsp.buf.hover, opts)
             end
 
             local format_on_save = A.augroup("Format_On_Save", { clear = true })
@@ -27,11 +28,12 @@ return {
                 group = format_on_save,
                 pattern = "*",
                 callback = function()
-                    vim.lsp.buf.format()
+                    vim.lsp.buf.format({ async = false })
                     vim.diagnostic.show()
                 end,
             })
 
+            -- Styling LSP
             local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
             for type, icon in pairs(signs) do
                 local hl = "DiagnosticSign" .. type
@@ -40,13 +42,8 @@ return {
 
             vim.diagnostic.config({
                 float = { border = "rounded" },
-                severity_sort = true,
-                signs = true,
-                underline = true,
-                update_in_insert = true,
                 virtual_text = true,
             })
-
             vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
                 vim.lsp.handlers.hover, { border = "rounded" }
             )
@@ -54,7 +51,7 @@ return {
                 vim.lsp.handlers.signature_help, { border = "rounded" }
             )
 
-            ---- SERVER CONFIGURATION
+            -- SERVER CONFIGURATIONS
             lspconfig["cssls"].setup({
                 capabilities = capabilities,
                 on_attach = on_attach,
@@ -66,6 +63,12 @@ return {
             lspconfig["hls"].setup({
                 capabilities = capabilities,
                 on_attach = on_attach,
+                filetypes = { "hs", "lhs", "haskell", "lhaskell" },
+                settings = {
+                    haskell = {
+                        formattingProvider = "fourmolu"
+                    }
+                }
             })
             lspconfig["jsonls"].setup({
                 capabilities = capabilities,
