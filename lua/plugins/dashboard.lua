f69a453f93e553a4009fc9f74c8397d261c937f6
ZacharyRizer <zacharyrizer@gmail.com>
2024-02-14 10:30:39 -0500

    Update to coc settings

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -8,7 +8,6 @@ return {
             config = {
                 center = {
                     { icon = "    ", desc = "Plugin Manager ", action = "Lazy" },
-                    { icon = "    ", desc = "LSP Manager ", action = "Mason" },
                     { icon = "    ", desc = "Config         ", action = "e ~/.config/nvim/init.lua" },
                 },
                 header = {
