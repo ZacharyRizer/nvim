c332f3eec0545ced828e799a285d74d2fef500d4
ZacharyRizer <zacharyrizer@gmail.com>
2024-02-14 10:21:57 -0500

    Swapped back to coc for simplicity

---
+++ lua/plugins/coc.lua
@@ -0,0 +1,57 @@
+return {
+    "neoclide/coc.nvim",
+    branch = "release",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        vim.g.coc_global_extensions = {
+            "coc-css",
+            "coc-git",
+            "coc-highlight",
+            "coc-html",
+            "coc-json",
+            "coc-lua",
+            "coc-marketplace",
+            "coc-prettier",
+            "coc-pyright",
+            "coc-rust-analyzer",
+            "coc-tsserver",
+            "coc-toml"
+        }
+
+        vim.cmd([[
+                function! CheckBackspace() abort
+                  let col = col('.') - 1
+                  return !col || getline('.')[col - 1]  =~# '\s'
+                endfunction
+
+                inoremap <silent><expr> <TAB> coc#pum#visible() ? coc#pum#next(1) : CheckBackspace() ? "\<Tab>" : coc#refresh()
+                inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+                inoremap <silent><expr> <CR> coc#pum#visible() ? coc#_select_confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+
+                nnoremap <silent><nowait><expr> <C-d> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-d>"
+                nnoremap <silent><nowait><expr> <C-u> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-u>"
+                inoremap <silent><nowait><expr> <C-d> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<Del>"
+                inoremap <silent><nowait><expr> <C-u> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(0)\<cr>" : ""
+            ]])
+
+        A.map("n", "K", ":call CocActionAsync('doHover')<CR>")
+
+        A.map("n", "gd", ":Telescope coc definitions<CR>")
+        A.map("n", "gi", ":Telescope coc implementations<CR>")
+        A.map("n", "gr", ":Telescope coc references<CR>")
+        A.map("n", "gt", ":Telescope coc type_definitions<CR>")
+        A.map("n", "<Leader>rn", "<Plug>(coc-rename)")
+
+        A.map("n", "<Leader>la", ":Telescope coc file_code_actions<CR>")
+        A.map("n", "<Leader>ld", ":Telescope coc diagnostics<CR>")
+        A.map("n", "<Leader>ls", ":Telescope treesitter<CR>")
+
+        A.map("n", "[d", "<Plug>(coc-diagnostic-prev)")
+        A.map("n", "]d", "<Plug>(coc-diagnostic-next)")
+
+        A.map("n", "[c", "<Plug>(coc-git-prevchunk)")
+        A.map("n", "]c", "<Plug>(coc-git-nextchunk)")
+        A.map("n", "gc", ":CocCommand git.chunkInfo<cr>")
+        A.map("n", "gb", ":CocCommand git.showBlameDoc<cr>")
+    end,
+}
